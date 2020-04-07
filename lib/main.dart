c6f048cc5f8e26df7e46d1c2045268fb4a3582b2
a1824jj <a1824jj@aiit.ac.jp>
2020-04-07 17:51:45 +0900

    play wav sound - use flutter package audioplayers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +12,13 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: Text(nouns.first)
+            child: FlatButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note1.wav');
+              },
+              child: Text('Click Me'),
+            ),
           ),
         ),
       ),
