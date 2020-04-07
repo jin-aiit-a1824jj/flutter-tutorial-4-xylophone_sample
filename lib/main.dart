dfec29b3bfc30d842f4ae55aeacdb381013ac93c
a1824jj <a1824jj@aiit.ac.jp>
2020-04-07 18:36:16 +0900

    fix ui & refactor

--- lib/main.dart
+++ lib/main.dart
@@ -10,58 +10,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              FlatButton(
-                color: Colors.red,
-                onPressed: () {
-                  playSound(1);
-                },
-                child: Text('Click Me'),
-              ),
-              FlatButton(
-                color: Colors.orange,
-                onPressed: () {
-                  playSound(2);
-                },
-                child: Text('Click Me'),
-              ),
-              FlatButton(
-                color: Colors.yellow,
-                onPressed: () {
-                  playSound(3);
-                },
-                child: Text('Click Me'),
-              ),
-              FlatButton(
-                color: Colors.green,
-                onPressed: () {
-                  playSound(4);
-                },
-                child: Text('Click Me'),
-              ),
-              FlatButton(
-                color: Colors.teal,
-                onPressed: () {
-                  playSound(5);
-                },
-                child: Text('Click Me'),
-              ),
-              FlatButton(
-                color: Colors.blue,
-                onPressed: () {
-                  playSound(6);
-                },
-                child: Text('Click Me'),
-              ),
-              FlatButton(
-                color: Colors.purple,
-                onPressed: () {
-                  playSound(7);
-                },
-                child: Text('Click Me'),
-              ),
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange,soundNumber: 2),
+              buildKey(color: Colors.yellow,soundNumber: 3),
+              buildKey(color: Colors.green,soundNumber: 4),
+              buildKey(color: Colors.teal,soundNumber: 5),
+              buildKey(color: Colors.blue,soundNumber: 6),
+              buildKey(color: Colors.purple,soundNumber: 7),
             ],
           ),
         ),
@@ -72,5 +32,15 @@ class MyApp extends StatelessWidget {
   void playSound(int soundNumber) {
     final player = AudioCache();
     player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({Color color, int soundNumber}){
+    return Expanded(
+        child: FlatButton(
+          color: color,
+          onPressed: () {playSound(soundNumber);},
+          child: Text('soundNumber -> $soundNumber')
+        )
+    );
   }
 }
